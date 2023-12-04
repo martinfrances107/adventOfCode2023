626a6b596c637c49e7ec7e8b761fae6b5036705c
martinfrances107 <martinfrances107@hotmail.com>
2023-12-04 08:23:25 +0000

    d4p1 stop using poweri() when left dift will do. Also Record the correct answer in the test.

--- day4/src/bin/part1.rs
+++ day4/src/bin/part1.rs
@@ -29,7 +29,9 @@ fn part1(input: &str) -> u32 {
                         }
                     }
 
-                    score = 2f32.powi(x - 1) as u32;
+                    if x != 0 {
+                        score = 1u32 << (x - 1)
+                    }
                 }
             }
             score
@@ -49,6 +51,6 @@ mod test {
         Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
         Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
         Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
-        assert_eq!(part1(input), 4361u32)
+        assert_eq!(part1(input), 13u32)
     }
 }
