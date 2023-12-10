714ec6620796bf0544eba7e3559e0e9fccc9d0ab
martinfrances107 <martinfrances107@hotmail.com>
2023-12-10 17:57:06 +0000

    Refactored to make the col and row indexing more explicit, lookat tarpaulin report, added new tests to get better coverage. ( I move the start position about and single stepped. No luck.

--- day10/src/bin/part1.rs
+++ day10/src/bin/part1.rs
@@ -1,41 +1,91 @@
+use core::fmt::Display;
+use core::fmt::Formatter;
+
 fn main() {
     let input = include_str!("./input1.txt");
-    // println!("{:?}", part1(input));
+    println!("{:?}", part1(input));
 }
 
-// Map symbols including blank.
-// pipe_elements = MAP_VALUE[0..=8]
-const MAP_VALUE: [char; 9] = ['-', '7', '|', 'J', '-', 'L', '|', 'F', '.'];
-
+#[derive(Debug)]
+struct Dir {
+    row_inc: i64,
+    col_inc: i64,
+}
 // clock rotation: N, E, S, W
-const DIRECTION: [(i16, i16); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
+// (row increment, col increment)
+const DIRECTION: [Dir; 4] = [
+    Dir {
+        row_inc: -1,
+        col_inc: 0,
+    },
+    Dir {
+        row_inc: 0,
+        col_inc: 1,
+    },
+    Dir {
+        row_inc: 1,
+        col_inc: 0,
+    },
+    Dir {
+        row_inc: 0,
+        col_inc: -1,
+    },
+];
+
+const DIGITS: [char; 10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 #[derive(Debug)]
 struct PipeMap {
     row: Vec<Vec<char>>,
-    start: (usize, usize),
+    start_row_index: usize,
+    start_col_index: usize,
+    output: Vec<Vec<char>>,
+    next_digit: usize,
 }
 
+impl Display for PipeMap {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        for r in &self.output {
+            for c in r.iter() {
+                write!(f, "{c}")?;
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+}
 #[derive(Debug, Eq, PartialEq)]
 struct State {
-    x: usize,
-    y: usize,
+    row_index: usize,
+    col_index: usize,
     blocked_direction: Option<usize>,
-    distance: u16,
+    distance: u32,
+}
+
+impl State {
+    fn position_match(&self, other: &Self) -> bool {
+        self.row_index == other.row_index && self.col_index == other.col_index
+    }
 }
 
 impl From<&str> for PipeMap {
     fn from(input: &str) -> Self {
-        let mut start: (usize, usize) = (usize::MAX, usize::MAX);
+        let mut start_row_index = None;
+        let mut start_col_index = None;
+        let mut output = vec![];
         let row = input
             .lines()
             .enumerate()
-            .map(|(y, line)| {
+            .map(|(row_index, line)| {
+                output.push(vec!['.'; line.len()]);
                 line.chars()
                     .enumerate()
-                    .map(|(x, c)| {
+                    .map(|(col_index, c)| {
                         if c == 'S' {
-                            start = (x, y);
+                            dbg!(row_index);
+                            dbg!(col_index);
+                            start_row_index = Some(row_index);
+                            start_col_index = Some(col_index);
                         }
                         c
                     })
@@ -43,51 +93,74 @@ impl From<&str> for PipeMap {
             })
             .collect::<Vec<Vec<char>>>();
 
-        Self { row, start }
+        match (start_row_index, start_col_index) {
+            (Some(start_row_index), Some(start_col_index)) => Self {
+                row,
+                start_row_index,
+                start_col_index,
+                output,
+                next_digit: 0,
+            },
+            _ => {
+                panic!("Must have found a start symbol");
+            }
+        }
     }
 }
 
 impl PipeMap {
-    fn walk(&self, state: State) -> State {
+    fn walk(&mut self, state: State) -> State {
         // walk round the clock looking for the exit.
         let mut next_blocked_direction = state.blocked_direction;
-        let mut x = i16::MAX;
-        let mut y = i16::MAX;
+        let mut row_index = 0usize;
+        let mut col_index = 0usize;
         for (i, d) in DIRECTION.iter().enumerate() {
             if Some(i) != state.blocked_direction {
-                x = state.x as i16 + d.0;
-                if x >= 0 {
-                    y = state.y as i16 + d.1;
-                    if y >= 0 {
-                        // The current pipe element under consideration.
-                        let pe = self.row[y as usize][x as usize];
-                        dbg!(x);
-                        dbg!(y);
-                        dbg!(pe);
+                row_index = (state.row_index as i64 + d.row_inc) as usize;
+                if let Some(row) = self.row.get(row_index) {
+                    col_index = (state.col_index as i64 + d.col_inc) as usize;
+                    // pe - The current pipe element under consideration.
+                    if let Some(pe) = row.get(col_index) {
                         // if we have found the exit and can compute the next
                         // backward direction
                         match i {
                             0 => {
-                                if pe == '|' || pe == 'F' || pe == '7' {
+                                if *pe == '|' || *pe == 'F' || *pe == '7' {
                                     next_blocked_direction = Some(2);
+                                    self.next_digit = (self.next_digit + 1) % 10;
+                                    if self.next_digit % 2 == 0 {
+                                        self.output[row_index][col_index] = DIGITS[self.next_digit];
+                                    }
                                     break;
                                 }
                             }
                             1 => {
-                                if pe == '-' || pe == '7' || pe == 'J' {
+                                if *pe == '-' || *pe == '7' || *pe == 'J' {
                                     next_blocked_direction = Some(3);
+                                    self.next_digit = (self.next_digit + 1) % 10;
+                                    if self.next_digit % 2 == 0 {
+                                        self.output[row_index][col_index] = DIGITS[self.next_digit];
+                                    }
                                     break;
                                 }
                             }
                             2 => {
-                                if pe == '|' || pe == 'J' || pe == 'L' {
+                                if *pe == '|' || *pe == 'J' || *pe == 'L' {
                                     next_blocked_direction = Some(0);
+                                    self.next_digit = (self.next_digit + 1) % 10;
+                                    if self.next_digit % 2 == 0 {
+                                        self.output[row_index][col_index] = DIGITS[self.next_digit];
+                                    }
                                     break;
                                 }
                             }
                             3 => {
-                                if pe == '-' || pe == 'F' || pe == 'L' {
+                                if *pe == '-' || *pe == 'F' || *pe == 'L' {
                                     next_blocked_direction = Some(1);
+                                    self.next_digit = (self.next_digit + 1) % 10;
+                                    if self.next_digit % 2 == 0 {
+                                        self.output[row_index][col_index] = DIGITS[self.next_digit];
+                                    }
                                     break;
                                 }
                             }
@@ -99,19 +172,62 @@ impl PipeMap {
                 }
             }
         }
+
+        // For debug fill the distance into the map
+        // self.row[y as usize][x as usize] = '*';
         State {
             distance: state.distance + 1,
-            x: x as usize,
-            y: y as usize,
+            row_index,
+            col_index,
             blocked_direction: next_blocked_direction,
         }
     }
 }
 
-// fn part1(input: &str) -> u32 {
-//         let map : PipeMap = input.into();
+fn part1(input: &str) -> u32 {
+    let mut map: PipeMap = input.into();
+    let mut first_walker = State {
+        distance: 0,
+        row_index: map.start_row_index,
+        col_index: map.start_col_index,
+        blocked_direction: None,
+    };
 
-// }
+    // Advance first machine into the pipe.
+    first_walker = map.walk(first_walker);
+
+    // which path should be block when starting the second walker?
+    dbg!(first_walker.blocked_direction);
+    let bd = match first_walker.blocked_direction {
+        Some(0) => Some(2),
+        Some(1) => Some(3),
+        Some(2) => Some(0),
+        Some(3) => Some(1),
+        _ => panic!("invalid block direction to turn around."),
+    };
+
+    let mut second_walker = State {
+        distance: 0,
+        row_index: map.start_row_index,
+        col_index: map.start_col_index,
+        blocked_direction: bd,
+    };
+    //To keep in sync also push the second walker down the pipe.
+    second_walker = map.walk(second_walker);
+
+    loop {
+        // Advance each walker until the share the same location again .. at the mid point of the loop.
+        first_walker = map.walk(first_walker);
+        second_walker = map.walk(second_walker);
+        if first_walker.position_match(&second_walker) {
+            break;
+        }
+    }
+    // dbg!(&first_walker);
+    assert_eq!(first_walker.distance, second_walker.distance);
+    println!("{map}");
+    first_walker.distance
+}
 
 #[cfg(test)]
 mod test {
@@ -125,15 +241,15 @@ mod test {
 SJ.L7
 |F--J
 LJ...";
-        let map: PipeMap = lines.into();
-        let start_pos = map.start;
-        assert_eq!(start_pos, (0, 2));
+        let mut map: PipeMap = lines.into();
+
         let start = State {
             distance: 0,
-            x: start_pos.0,
-            y: start_pos.1,
+            row_index: map.start_row_index,
+            col_index: map.start_col_index,
             blocked_direction: None,
         };
+
         let first_path_state = map.walk(start);
         dbg!(&first_path_state);
 
@@ -141,16 +257,16 @@ LJ...";
             first_path_state,
             State {
                 distance: 1,
-                x: 1,
-                y: 2,
+                row_index: 2,
+                col_index: 1,
                 blocked_direction: Some(3)
             }
         );
 
         let second_start = State {
             distance: 0,
-            x: start_pos.0,
-            y: start_pos.1,
+            row_index: map.start_row_index,
+            col_index: map.start_col_index,
             blocked_direction: Some(1),
         };
         let second_path_state = map.walk(second_start);
@@ -158,20 +274,118 @@ LJ...";
             second_path_state,
             State {
                 distance: 1,
-                x: 0,
-                y: 3,
+                row_index: 3,
+                col_index: 0,
                 blocked_direction: Some(0)
             }
         );
     }
 
-    // #[test]
-    // fn example() {
-    //     let input = r"
-    //     1abc2
-    //     pqr3stu8vwx
-    //     a1b2c3d4e5f
-    //     treb7uchet";
-    //     assert_eq!(part1(input), 142u32)
-    // }
+    #[test]
+    fn single_step_edge_case() {
+        let lines = r"..F7.
+.FJ|.
+FJ.L7
+|F--S
+LJ...";
+
+        let mut map: PipeMap = lines.into();
+
+        assert_eq!(map.start_row_index, 3);
+        assert_eq!(map.start_col_index, 4);
+        let start = State {
+            distance: 0,
+            row_index: map.start_row_index,
+            col_index: map.start_col_index,
+            blocked_direction: None,
+        };
+
+        let first_path_state = map.walk(start);
+        dbg!(&first_path_state);
+
+        assert_eq!(
+            first_path_state,
+            State {
+                distance: 1,
+                row_index: 2,
+                col_index: 4,
+                blocked_direction: Some(2)
+            }
+        );
+
+        let second_start = State {
+            distance: 0,
+            row_index: map.start_row_index,
+            col_index: map.start_col_index,
+            blocked_direction: Some(0),
+        };
+        let second_path_state = map.walk(second_start);
+        assert_eq!(
+            second_path_state,
+            State {
+                distance: 1,
+                row_index: 3,
+                col_index: 3,
+                blocked_direction: Some(1)
+            }
+        );
+
+        let dist1 = part1(lines);
+        let dist2 = part1(lines);
+
+        assert_eq!(dist1, 8);
+        assert_eq!(dist2, 8);
+    }
+
+    #[test]
+    fn example1() {
+        let lines = r".....
+.S-7.
+.|.|.
+.L-J.
+.....";
+
+        assert_eq!(part1(lines), 4);
+    }
+
+    // Same patter as example1 but which clutter.
+    fn obsquered_loop() {
+        let lines = r"-L|F7
+7S-7|
+L|7||
+-L-J|
+L|-JF";
+
+        assert_eq!(part1(lines), 4);
+    }
+
+    #[test]
+    fn two_loops_unknown_length() {
+        let loop1 = r"..F7.
+.FJ|.
+SJ.L7
+|F--J
+LJ...";
+
+        let loop2 = r"7-F7-
+.FJ|7
+SJLL7
+|F--J
+LJ.LJ";
+
+        let dist1 = part1(loop1);
+        let dist2 = part1(loop2);
+
+        assert_eq!(dist1, dist2);
+    }
+    #[test]
+    fn example2() {
+        let lines = r"..F7.
+.FJ|.
+SJ.L7
+|F--J
+LJ...";
+
+        assert_eq!(part1(lines), 8);
+    }
 }
