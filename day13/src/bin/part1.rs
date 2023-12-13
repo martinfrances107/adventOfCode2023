90e6eee65c6ce1e2e17695d0d4dd815c8be2a958
martinfrances107 <martinfrances107@hotmail.com>
2023-12-13 15:56:59 +0000

    Day12p1: Have initial function, working towards first star.

--- day13/src/bin/part1.rs
+++ day13/src/bin/part1.rs
@@ -1,14 +1,18 @@
+use core::iter::successors;
+
 fn main() {
     let input = include_str!("./input1.txt");
     println!("{:?}", part1(input));
 }
 
-fn col_map(row_map: Vec<&str>) -> Vec<Vec<char>> {
+fn col_map(row_map: Vec<Vec<char>>) -> Vec<Vec<char>> {
     let row_len = row_map[0].len();
-    let mut col_map: Vec<Vec<char>> = Vec::with_capacity(row_len);
+    // let mut col_map: Vec<Vec<char>> = Vec::with_capacity(row_len);
+    // Every column needs to be defined before population.
+    let mut col_map: Vec<Vec<char>> = (1..=row_len).map(|_| vec![]).collect();
     for row in row_map.iter() {
-        for (col_index, c) in (*row).chars().enumerate() {
-            col_map[col_index].push(c);
+        for (col_index, c) in (*row).iter().enumerate() {
+            col_map[col_index].push(*c);
         }
     }
     col_map
@@ -22,12 +26,37 @@ fn part1(input: &str) -> u32 {
     todo!();
 }
 
-fn row_matches(row_map: &Vec<&str>) -> Vec<(usize, usize)> {
-    todo!();
+fn row_matches(row_map: Vec<Vec<char>>) -> Vec<(u128, u128)> {
+    row_map
+        .windows(2)
+        .enumerate()
+        .filter_map(|(row_index, row_pair)| {
+            if *row_pair[0] == *row_pair[1] {
+                // convert col_index into column number
+                // index: zero based counting
+                Some((row_index as u128 + 1, (row_index + 2) as u128))
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>()
 }
 
-fn col_matches(col_map: &Vec<Vec<char>>) -> Vec<(usize, usize)> {
-    todo!();
+fn col_matches(col_map: Vec<Vec<char>>) -> Vec<(u128, u128)> {
+    col_map
+        .windows(2)
+        .enumerate()
+        .filter_map(|(col_index, col_pair)| {
+            if *col_pair[0] == *col_pair[1] {
+                // convert col_index into column number
+                // index: zero based counting
+                // col number starts at one.
+                Some(((col_index + 1) as u128, (col_index + 2) as u128))
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>()
 }
 
 #[cfg(test)]
@@ -37,19 +66,6 @@ mod test {
 
     #[test]
     fn horizontal() {
-        let input = r"#.##..##.
-      ..#.##.#.
-      ##......#
-      ##......#
-      ..#.##.#.
-      ..##..##.
-      #.#.##.#.";
-        let row_map = input.lines().map(|line| line).collect::<Vec<_>>();
-        assert_eq!(row_matches(&row_map), vec![(4, 5)])
-    }
-
-    #[test]
-    fn vertical() {
         let input = r"#...##..#
 #....#..#
 ..##..###
@@ -57,8 +73,28 @@ mod test {
 #####.##.
 ..##..###
 #....#..#";
-        let row_map = input.lines().map(|line| line).collect::<Vec<_>>();
+        let row_map = input
+            .lines()
+            .map(|line| line.chars().collect::<Vec<char>>())
+            .collect::<Vec<_>>();
+
+        assert_eq!(row_matches(row_map), vec![(4, 5)])
+    }
+
+    #[test]
+    fn vertical() {
+        let input = r"#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.";
+        let row_map = input
+            .lines()
+            .map(|line| line.chars().collect::<Vec<char>>())
+            .collect::<Vec<Vec<char>>>();
         let col_map = col_map(row_map);
-        assert_eq!(col_matches(&col_map), vec![(5, 6)]);
+        assert_eq!(col_matches(col_map), vec![(5, 6)]);
     }
 }
